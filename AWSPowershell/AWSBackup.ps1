b16dfc5400983a9b96195d0d7d8c14b92d0eea48
S1ntaxErr0r <33460245+S1ntaxErr0r@users.noreply.github.com>
2019-12-20 10:17:17 -0500

    feat: initial upload

---
+++ AWSPowershell/AWSBackup.ps1
@@ -0,0 +1,57 @@
+﻿function Backup-AWS {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String[]]
+        $BackupifyList,
+        $BucketName
+    )
+
+
+$addresses = Import-CSV $BackupifyList
+$Root = Get-Date -UFormat "%B_%Y"
+
+foreach ($address in $addresses){
+
+    $url = $address.archive_url
+    $Folder = $address.service_type
+    $Folder = $Folder -replace ".{7}$"
+
+
+        $start_time = Get-Date
+
+        $url -match 'backupify_export(.*?)zip'
+
+        $filename = $matches[0]
+        $filename = $filename.Replace('%40','@')
+
+        $output = "D:\Backupify\Data\$filename"
+
+        $ProgressPreference = 'SilentlyContinue'
+
+        if(Test-Path $output){
+
+        Write-Host "File already exist."
+            
+            }
+
+         Else{
+                 
+        Invoke-WebRequest -Uri $url -OutFile $output
+            
+            }
+
+        Write-Output "Time taken to download $($filename): $((Get-Date).Subtract($start_time).Minutes) Minutes(s)"
+
+        $Key = "$Root/$Folder/$filename"
+
+        Write-S3Object -BucketName $BucketName -File $output -Key $Key -Credential $sessionCreds
+		
+		Write-Output "Time taken to upload $($filename): $((Get-Date).Subtract($start_time).Minutes) Minutes(s)"
+
+
+        Remove-Item $output
+
+}
+
+}
+Export-ModuleMember -Function Backup-AWS
---
+++ AWSPowershell/AWSMFA.ps1
@@ -0,0 +1,20 @@
+﻿function Invoke-AWSAssumeRole {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $AWSProfile,
+        $MFAProfile,
+        $MFARole
+    )
+
+do{
+    $MFACode = Read-Host "Please enter a valid 6 digit MFA Code:"
+    }
+    while($MFACode.Length -ne 6)
+
+Initialize-AWSDefaults -ProfileName $AWSProfile
+$global:sessionCreds = (Use-STSRole -RoleArn $MFARole -SerialNumber $MFAProfile -TokenCode $MFACode -DurationInSeconds 43200 -RoleSessionName "AssumedRole").Credentials
+
+}
+
+Export-ModuleMember -Function Invoke-AWSAssumeRole
