c34de10e2766a9d6e287ec1d6c14ad7032855942
S1ntaxErr0r <33460245+S1ntaxErr0r@users.noreply.github.com>
2020-05-05 09:08:41 -0400

    Add files via upload

---
+++ NDI-AWSPowerShell/AWSMFA.ps1
@@ -0,0 +1,35 @@
+﻿function Invoke-AWSAssumeRole {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]
+        $AWSProfile,
+        $MFAProfile,
+        $MFARole
+
+    )
+
+    $AWSCreds = "$env:USERPROFILE\.aws\credentials"
+    $Credentials = Get-Content -Path $env:USERPROFILE\.aws\credentials
+
+    do{
+        $MFACode = Read-Host "Please enter a valid 6 digit MFA Code:"
+        }
+        while($MFACode.Length -ne 6)
+    
+    $global:sessionInfo = aws sts assume-role --role-arn $($MFARole) --role-session-name Backupify --serial-number $($MFAProfile) --token-code $($MFACode) --duration-seconds 43200
+    
+    $sessionHash = $sessionInfo | ConvertFrom-Json
+    
+    $AccessKey = $sessionHash.Credentials.AccessKeyId
+    $SecretKey = $sessionHash.Credentials.SecretAccessKey
+    $Token = $sessionHash.Credentials.SessionToken
+    
+    $Credentials[11] = "aws_access_key_id = $AccessKey"
+    $Credentials[12] = "aws_secret_access_key = $SecretKey"
+    $Credentials[13] = "aws_session_token = $Token"
+    
+    $Credentials | Set-Content -Path $AWSCreds
+
+    }
+
+Export-ModuleMember -Function Invoke-AWSAssumeRole
