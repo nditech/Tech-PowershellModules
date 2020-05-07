0ea9e34e0ab2d5bee5cfde4881629d9bffc41bea
S1ntaxErr0r <33460245+S1ntaxErr0r@users.noreply.github.com>
2020-05-07 08:55:23 -0400

    Added Zip flag for archiving

--- NDI-ProfileMigration/BackupUserProfile.ps1
+++ NDI-ProfileMigration/BackupUserProfile.ps1
@@ -2,9 +2,8 @@ function Backup-UserProfile {
 
     param (
         # Parameter help description
-        [Parameter()]
-        [string]
-        $Destination = "C:\Backup"
+        [Parameter()][string]$Destination = "C:\Backup",
+        [Parameter()][switch]$Zip = $false
     )
 
     write-host -ForegroundColor Green "Backing up data from local machine for $Username"
@@ -12,11 +11,18 @@ function Backup-UserProfile {
         foreach ($Folder in $Folders){
 
             $LocalFolder = $SelectedProfile + "\" + $Folder
-            $BackupFolder = $Destination + "\" + $username + "\" + $Folder
+            $BackupFolder = $Destination + "\" + $Username + "\" + $Folder
             $FolderSize = (Get-ChildItem -ErrorAction silentlyContinue $LocalFolder -Recurse -Force | Measure-Object -ErrorAction silentlyContinue -Property Length -Sum ).Sum / 1MB
             $FolderSizeRounded = [System.Math]::Round($FolderSize)
             write-host -ForegroundColor cyan "  $Folder... ($FolderSizeRounded MB)"
             Copy-Item -ErrorAction silentlyContinue -recurse $LocalFolder $BackupFolder
+        }
+    
+        if($Zip){
+        Write-Host -ForegroundColor Green "Creating Archive"
+
+        $Desktop = [Environment]::GetFolderPath("Desktop")
+        Compress-Archive -Path $Destination\$Username -DestinationPath $Desktop\$Username.zip
         }
 
 }
