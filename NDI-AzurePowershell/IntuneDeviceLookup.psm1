6a9a8ebdbf4b15e3e62db6645eb3e1e203e9a207
S1ntaxErr0r <33460245+S1ntaxErr0r@users.noreply.github.com>
2020-07-29 11:21:36 -0400

    Add files via upload

--- NDI-AzurePowershell/IntuneDeviceLookup.psm1
+++ NDI-AzurePowershell/IntuneDeviceLookup.psm1
@@ -36,3 +36,48 @@ function Get-IntuneDevice {
 
     $customObj | Format-Table -AutoSize
 }
+function Get-AllIntuneDevice {
+
+  <#
+  .SYNOPSIS
+    Users a devices serial number to query Azure Active Directory/Intune for the last logged on user.
+    
+  .DESCRIPTION
+    The Get-IntuneDevice function requires to parameters. (1) Serial Number and (2) Username. The username
+    input is necessary to authenticate with MSGraph and Azure.
+    
+    Requires the following modules:
+    Import-Module -Name MSGraphFunctions
+    Import-Module -Name Microsoft.Graph.Intune
+     
+  .EXAMPLE
+    Get-IntuneDevice -SerialNumber "XXXXXX" -Username "jdoe@ndi.org"
+  #>
+
+  param (
+      [Parameter(Mandatory=$true)]
+      [String[]]
+      $Username
+  )
+  
+  Connect-Graph -Username $Username
+  $Output = @()
+  $IntuneDevices = Get-GraphManagedDevice
+
+    foreach ($Device in $IntuneDevices){
+    $IntuneUser = Get-GraphUsersLoggedOn -Id $IntuneDevice.id
+
+      $customObj = [PSCustomObject]@{
+          SystemName = $Device.deviceName
+          User = $IntuneUser.userDisplayName
+          LastLogonDate = $IntuneUser.lastLogonDateTime
+          SerialNumber = $Device.serialNumber
+      }
+    
+    $Output += $customObj
+
+    }
+
+  $Output | Format-Table -AutoSize
+
+}
